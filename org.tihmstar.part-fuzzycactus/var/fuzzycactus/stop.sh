be01eb1cf5416abeb9952e825ed10bb4d0046ce1
tihmstar <tihmstar@gmail.com>
2013-10-24 17:26:25 +0200

    added testfiles

---
+++ org.tihmstar.part-fuzzycactus/var/fuzzycactus/stop.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+pnum=$( ps -ax | grep fuzzycactus | grep -c -v grep )
+
+if [ $pnum -eq 2 ]; then
+        kill "$(  fuzz=( $( ps -ax | grep fuzzycactus | grep -v grep ) ) && echo ${fuzz[0]} )" &> /dev/null
+        kill "$(  fuzz=( $( ps -ax | grep fuzzycactus | grep -v grep ) ) && echo ${fuzz[0]} )" &> /dev/null
+        echo "Stopped."
+elif [ $pnum -eq 1 ]; then
+	kill "$(  fuzz=( $( ps -ax | grep fuzzycactus | grep -v grep ) ) && echo ${fuzz[0]} )" &> /dev/null
+	echo "Stopped."
+elif [[ $pnum -eq 0 ]]; then
+	echo "No fuzzycactus process running."
+else
+	echo "Multiple instances of fuzzycactus found, please kill them manually."
+	echo "$( ps -ax | grep fuzzycactus | grep -v grep )"
+fi
+
+if [[ ! -e ./crashclean.sh ]]; then
+	echo "No 'crashclean.sh' file found, please re-download from http://github.com/compilingEntropy/fuzzycactus."
+	exit
+fi
+./crashclean.sh 2>&1 | tee -a ./fuzz.log
